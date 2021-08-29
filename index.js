019f274740a971c51556598fb241ee742ae6b4f2
SayantikaMaitra <86530429+SayantikaMaitra@users.noreply.github.com>
2021-08-29 19:15:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,75 +1,24 @@
-//button pressed
-var numberOfDrumButton = document.querySelectorAll(".drum").length;
-
-for (var i = 0; i < numberOfDrumButton; i++) {
-
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
-    var buttonInnerHTML = this.innerHTML;
-    makeSound(buttonInnerHTML);
-buttonAnimation(buttonInnerHTML);
-  });
-
-}
-//key pressed
-document.addEventListener ("keypress",function(event) {
-makeSound(event.key);
-buttonAnimation(event.key);
-});
-
-function makeSound(key){
-  switch (key) {
-    case "w":
-    var tom1 = new Audio("sounds/tom-1.mp3");
-    tom1.play();
-    break;
-
-  case "a":
-    var tom2 = new Audio("sounds/tom-2.mp3");
-    tom2.play();
-    break;
-
-  case "s":
-    var tom3 = new Audio('sounds/tom-3.mp3');
-    tom3.play();
-    break;
-
-  case "d":
-    var tom4 = new Audio('sounds/tom-4.mp3');
-    tom4.play();
-    break;
-
-  case "j":
-    var snare = new Audio('sounds/snare.mp3');
-    snare.play();
-    break;
-
-  case "k":
-    var crash = new Audio('sounds/crash.mp3');
-    crash.play();
-    break;
-
-  case "l":
-    var kick = new Audio('sounds/kick-bass.mp3');
-    kick.play();
-    break;
 
 
-  default: console.log(key);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-  }
-}
+var image1 = document.querySelectorAll("img")[0];
 
-function buttonAnimation(currentKey) {
+image1.setAttribute("src", randomImageSource);
 
-  var activeButton = document.querySelector("." + currentKey);
-
-  activeButton.classList.add("pressed");
-
-  setTimeout(function() {
-    activeButton.classList.remove("pressed");
-  }, 100);
-
-}
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+ if (randomNumber1 > randomNumber2){
+   document.querySelector("h1").innerHTML = "❤Player 1 Wins!!";
+ }
+ else if (randomNumber2>randomNumber1){
+   document.querySelector("h1").innerHTML = "❤ Player 2 Wins!!";
+ }
+ else {
+   document.querySelector("h1").innerHTML = "Draw!!!!!!!!!";
+ }
